ddb8f034bdedd0f56f03f51c6abacafb40ed0bcd
Hexagon <77287556+Hexagonz@users.noreply.github.com>
2021-04-13 22:56:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-
+// SC NYA JANGAN DI JUAL NGENTOD
 //MAKASIH DAH PAKE 
-//HEXAGON NGENTOD EMANG
 const
 	{
 		WAConnection,
@@ -669,84 +668,58 @@ Prefix : 「 ${prefix} 」
     case prefix+ 'stiker':
     case prefix+ 'sg':
     case prefix+ 's':
-          if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
-            const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
+            if ((isMedia && !mek.message.videoMessage || isQuotedImage) && args.length == 0) {
+            const encmedia = isQuotedImage ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
             const media = await hexa.downloadAndSaveMediaMessage(encmedia)
-            ran = getRandom('.webp')
-            await ffmpeg(`./${media}`)
-              .input(media)
-              .on('start', function (cmd) {
-                console.log(`Started : ${cmd}`)
-              })
-              .on('error', function (err) {
-                console.log(`Error : ${err}`)
+                ran = '666.webp'
+                await ffmpeg(`./${media}`)
+                .input(media)
+                .on('start', function (cmd) {
+                     console.log(`Started : ${cmd}`)
+                })
+                .on('error', function (err) {
+                 console.log(`Error : ${err}`)
                 fs.unlinkSync(media)
-                reply(mess.error.stick)
-              })
-              .on('end', function () {
+                reply('error')
+                })
+                .on('end', function () {
                 console.log('Finish')
-                hexa.sendMessage(from, fs.readFileSync(ran), sticker, { quoted:mek}) 
-                fs.unlinkSync(media)
+                hexa.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
+                 fs.unlinkSync(media)
                 fs.unlinkSync(ran)
-              })
-              .addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-              .toFormat('webp')
-              .save(ran)
-          } else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
-            const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-            const media = await hexa.downloadAndSaveMediaMessage(encmedia)
-            ran = getRandom('.webp')
+                })
+                .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+                .toFormat('webp')
+                .save(ran)
+                } else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
+                const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM', 'm')).message.extendedTextMessage.contextInfo : mek
+                const media = await hexa.downloadAndSaveMediaMessage(encmedia)
+            ran = '999.webp'
             reply(mess.wait)
             await ffmpeg(`./${media}`)
-              .inputFormat(media.split('.')[1])
-              .on('start', function (cmd) {
-                console.log(`Started : ${cmd}`)
-              })
-              .on('error', function (err) {
-                console.log(`Error : ${err}`)
-                fs.unlinkSync(media)
-                tipe = media.endsWith('.mp4') ? 'video' : 'gif'
-                reply(`❌ Gagal, pada saat mengkonversi ${tipe} ke stiker`)
-              })
-              .on('end', function () {
-                console.log('Finish')
-                hexa.sendMessage(from, fs.readFileSync(ran), sticker, {quoted:mek})
-                fs.unlinkSync(media)
-                fs.unlinkSync(ran)
-              })
-              .addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-              .toFormat('webp')
-              .save(ran)
-          } else if ((isMedia && mek.message.videoMessage.seconds < 11 || isQuotedVideo && mek.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11) && args.length == 0) {
-            const encmedia = isQuotedVideo ? JSON.parse(JSON.stringify(mek).replace('quotedM','m')).message.extendedTextMessage.contextInfo : mek
-            const media = await hexa.downloadAndSaveMediaMessage(encmedia)
-            ran = getRandom('.webp')
-            reply(mess.wait)
-            await ffmpeg(`./${media}`)
-              .inputFormat(media.split('.')[1])
-              .on('start', function (cmd) {
-                console.log(`Started : ${cmd}`)
-              })
-              .on('error', function (err) {
-                console.log(`Error : ${err}`)
-                fs.unlinkSync(media)
-                tipe = media.endsWith('.mp4') ? 'video' : 'gif'
-                reply(`Yah gagal, coba ulangi ^_^`)
-              })
-              .on('end', function () {
-                console.log('Finish')
-                buff = fs.readFileSync(ran)
-                hexa.sendMessage(from, buff, sticker,{quoted:mek})
-                fs.unlinkSync(media)
-                fs.unlinkSync(ran)
+            .inputFormat(media.split('.')[1])
+            .on('start', function (cmd) {
+            console.log(`Started : ${cmd}`)
             })
-            .addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
-            .toFormat('webp')
-            .save(ran)
+            .on('error', function (err) {
+            console.log(`Error : ${err}`)
+            fs.unlinkSync(media)
+            tipe = media.endsWith('.mp4') ? 'video' : 'gif'
+            reply(`Gagal, pada saat mengkonversi ${tipe} ke stiker`)
+            })
+            .on('end', function () {
+            console.log('Finish')
+            hexa.sendMessage(from, fs.readFileSync(ran), sticker, {quoted: mek})
+            fs.unlinkSync(media)
+            fs.unlinkSync(ran)
+                })
+                .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+                .toFormat('webp')
+                .save(ran)
             } else {
-            reply(`Kirim gambaar dengan caption ${prefix}sticker atau reply/tag gambar`)
-          	}
-          	break              
+                reply(`Kirim gambar dengan caption ${prefix}sticker\nDurasi Sticker Video 1-9 Detik`)
+            }
+            break               
     case prefix+ 'toimg':
 			if (!isQuotedSticker) return reply('𝗥𝗲𝗽𝗹𝘆/𝘁𝗮𝗴 𝘀𝘁𝗶𝗰𝗸𝗲𝗿 !')
 			reply(mess.wait)
